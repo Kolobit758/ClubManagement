15940b4ddc60807c898a9960cedb47a5fc062c12
Kolobit758 <kobchokbn@gmail.com>
2026-04-29 15:03:15 +0700

    Update google form

--- js/admin.js
+++ js/admin.js
@@ -9,3 +9,16 @@ async function loadStats() {
     `นักเรียน ${students} คน <br> ชุมนุม ${clubs} ห้อง`;
 }
 loadStats();
+
+async function createGoogleForm() {
+
+  const { data } = await db.from("clubs").select("name");
+
+  const clubs = data.map(c => c.name).join(",");
+
+  const url =
+    "https://script.google.com/macros/s/AKfycbzAKDucxvtJDQ_xTQXQDAjm0ApG6fjg62vV9T72aJ42BmqLtXhWXbHZlvSrojg3zAj9/exec"
+    + encodeURIComponent(clubs);
+
+  window.open(url, "_blank");
+}
